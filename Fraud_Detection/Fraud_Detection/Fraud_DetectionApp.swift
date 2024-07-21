428b4f8e5795e3f84d7a76c0671a01e1c5229314
Dheeraj Rathore <dheeraj.rathore@rapipay.com>
2024-07-21 12:47:47 +0530

    Xcode project created.

---
+++ Fraud_Detection/Fraud_Detection/Fraud_DetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fraud_DetectionApp.swift
+//  Fraud_Detection
+//
+//  Created by Dheeraj Rathore  on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Fraud_DetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
